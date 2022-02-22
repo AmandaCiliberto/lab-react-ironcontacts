43e0944a50f762daab4f426602e33ff5207d0967
Amanda Rodrigues <amanda.rodrigues@lego.com>
2022-02-22 15:37:26 +0100

    Iteration 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import "./App.css";
+import { useState } from "react"; // <-- add
+import contacts from "./contacts.json";
 
 function App() {
+  const [contact, setContacts] = useState(contacts.slice(0, 5));
+  console.log(contact);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={"App " + contact}>
+      <h1>Iron Contacts</h1>
+      <div>
+        <table>
+          <tr>
+            <th> Picture </th>
+            <th> Name </th>
+            <th> Popularity </th>
+          </tr>
+          {contact.map(function (contact) {
+            return (
+              <tr key={contact.id}>
+                <td><img id="face" src = {contact.pictureUrl} alt="contact face" /></td>
+                <td> {contact.name} </td>
+                <td> {contact.popularity} </td>
+              </tr>
+            );
+          })}
+        </table>
+      </div>
     </div>
   );
 }
